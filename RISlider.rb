e6097b6f04db374f0837dfbf7fbf81ecef20c7a1
lukecrum20 <lukecrum20@gmail.com>
2018-06-02 12:40:39 -0400

    fix slider

--- RISlider.rb
+++ RISlider.rb
@@ -4,19 +4,22 @@ class RISlider
     extend Ruby2D::DSL
     set background: 'white'
     set title: 'UI Testing'
+    square_x = x
+    square_y = y
     @line = Line.new(x1: x, y1: y, x2: x + line_length, y2: y, color: 'black')
-    @square = Square.new(size: square_size, x: x , y: y - (square_size/2), color: 'green')
+    @square = Square.new(size: square_size, x: square_x, y: square_y - (square_size/2), color: 'green')
     @active = false
     @holding = false
     @x = x
-    @orig_x = x
     @max = x + line_length
+    @min = x
+    @value = 0
     @down_action = on :mouse_down do |e|
       #check if on square
       #set square's x to mouse's x, only while held down
-      @holding = true
       if @square.contains?(e.x, e.y)
          @active = true
+         @holding = true
          @x = e.x
       end
     end
@@ -29,15 +32,18 @@ class RISlider
       extend Ruby2D::DSL
       if @holding & @active
         mouse_x = get :mouse_x
-        unless mouse_x > @max
+        if mouse_x < @max && mouse_x > @min
           @square.x = get :mouse_x
-        else
+        elsif mouse_x > @max
           @square.x = @max
-        end
-        if @square.x < (@x - @square.size)
-          @square.x = @x - @square.size
+        elsif @square.x < (@min - @square.size)
+          @square.x = @min - @square.size
         end
       end
+      @value = (@square.x - @min)/10 #10 ticks by default
+      puts "Value: #{@value}"
+      puts "Square x: #{@square.x}"
+      puts "X: #{@x}"
     end
   end
 end
