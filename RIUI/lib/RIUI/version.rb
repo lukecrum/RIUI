109d23a02d95172435274728f7fc3597043292cb
lukecrum20 <lukecrum20@gmail.com>
2018-06-11 16:37:13 -0700

    gem stuff

---
+++ RIUI/lib/RIUI/version.rb
@@ -0,0 +1,3 @@
+module RIUI
+  VERSION = "0.1.0"
+end
---
+++ RIUI/spec/RIUI_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RIUI do
+  it "has a version number" do
+    expect(RIUI::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
